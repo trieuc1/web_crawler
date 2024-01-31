08b59904ec0a1d182f4782b8dfd04a6d2a138b6b
Dean Nguyen <deanpn@uci.edu>
2024-01-31 09:17:28 -0800

    Write to txt

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ import logging
 import re
 from urllib.parse import urlparse, urljoin
 from lxml import html, etree
-
+from pathlib import Path
 logger = logging.getLogger(__name__)
 
 class Crawler:
@@ -20,15 +20,21 @@ class Crawler:
         This method starts the crawling process which is scraping urls from the next available link in frontier and adding
         the scraped links to the frontier
         """
-        while self.frontier.has_next_url():
-            url = self.frontier.get_next_url()
-            logger.info("Fetching URL %s ... Fetched: %s, Queue size: %s", url, self.frontier.fetched, len(self.frontier))
-            url_data = self.corpus.fetch_url(url)
+        
+        Path.touch("url_links.txt")
+        with Path.open("ur_link.txt", "a") as txt:
+            while self.frontier.has_next_url():
+                url = self.frontier.get_next_url()
+                logger.info("Fetching URL %s ... Fetched: %s, Queue size: %s", url, self.frontier.fetched, len(self.frontier))
 
-            for next_link in self.extract_next_links(url_data):
-                if self.is_valid(next_link):
-                    if self.corpus.get_file_name(next_link) is not None:
-                        self.frontier.add_url(next_link)
+                url_data = self.corpus.fetch_url(url)
+                # Write links to txt
+                
+                txt.write(url + "\n")
+                for next_link in self.extract_next_links(url_data):
+                    if self.is_valid(next_link):
+                        if self.corpus.get_file_name(next_link) is not None:
+                            self.frontier.add_url(next_link)
 
     def extract_next_links(self, url_data):
         """
@@ -69,14 +75,19 @@ class Crawler:
         if parsed.scheme not in set(["http", "https"]):
             return False
         try:
-            return ".ics.uci.edu" in parsed.hostname \
+            if ( ".ics.uci.edu" in parsed.hostname \
                    and not re.match(".*\.(css|js|bmp|gif|jpe?g|ico" + "|png|tiff?|mid|mp2|mp3|mp4" \
                                     + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf" \
                                     + "|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1" \
                                     + "|thmx|mso|arff|rtf|jar|csv" \
-                                    + "|rm|smil|wmv|swf|wma|zip|rar|gz|pdf)$", parsed.path.lower())
-
+                                    + "|rm|smil|wmv|swf|wma|zip|rar|gz|pdf)$", parsed.path.lower())):
+                return True
+            else:
+                return False
+            
         except TypeError:
             print("TypeError for ", parsed)
             return False
+        
+
 
