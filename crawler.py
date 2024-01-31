7441154255cf58c145bcdf52c85a6abe135ef525
Celine Trieu <trieuc1@uci.edu>
2024-01-31 13:43:43 -0800

    Fixed bug in is_path_valid

--- crawler.py
+++ crawler.py
@@ -1,9 +1,10 @@
 import logging
+from bs4 import BeautifulSoup
 import re
 from urllib.parse import urlparse, urljoin, parse_qsl, urlunparse
 from lxml import html, etree
 from pathlib import Path
-import requests
+# import requests
 logger = logging.getLogger(__name__)
 
 class Crawler:
@@ -15,6 +16,7 @@ class Crawler:
     def __init__(self, frontier, corpus):
         self.frontier = frontier
         self.corpus = corpus
+        self.subdomains = dict()
 
     def start_crawling(self):
         """
@@ -22,8 +24,8 @@ class Crawler:
         the scraped links to the frontier
         """
         
-        Path.touch("url_links.txt")
-        with Path.open("ur_link.txt", "a") as txt:
+        Path.touch("url_link.txt")
+        with open("url_link.txt", "a", encoding='utf-8') as txt:
             while self.frontier.has_next_url():
                 url = self.frontier.get_next_url()
                 logger.info("Fetching URL %s ... Fetched: %s, Queue size: %s", url, self.frontier.fetched, len(self.frontier))
@@ -35,7 +37,7 @@ class Crawler:
                 for next_link in self.extract_next_links(url_data):
                     if self.is_valid(next_link):
                         #if a link is valid, it will grab all the words in the file, returns it as a list
-                        all_words_in_file = self.words_in_link(next_link)
+                        # all_words_in_file = self.extract_words_generator(next_link)
                         #print(all_words_in_file)
 
                         if self.corpus.get_file_name(next_link) is not None:
@@ -81,8 +83,7 @@ class Crawler:
         where parsed_url = urlparse(url) object
         """
         # if there is not path
-        if parsed_url.path == "":
-            print("eller")
+        if parsed_url.path == "" or parsed_url.path == '/':
             return True
         
         # gets url pieces w/o query and fragment
@@ -99,14 +100,11 @@ class Crawler:
         path_lst = path.split('/')
         clean_path_lst = [i for i in path_lst if (i != "")]
         last_path = clean_path_lst[len(clean_path_lst) - 1]
-        print(clean_path_lst)
-        print(last_path)
         # get file and folder from path
         if "." in last_path:
             if len(clean_path_lst) > 1:
                 folder = clean_path_lst[-2]
                 file = last_path.split('.')[0]
-                print(folder)
             else:
                 file = last_path.split('.')[0]
                 folder = ""
@@ -123,7 +121,6 @@ class Crawler:
                     return True
             return False
         if folder != "" and file != "":
-            print("up")
             if folder.isalnum():
                 num = self.count_num(folder)
                 if num / len(folder) >= 0.5:
@@ -136,27 +133,15 @@ class Crawler:
                         return True
             return False
         return False
-
-    def words_in_link(self, url_content):
+            
+    def extract_words_generator(self, url_data):
         """
         Function that gets the file content. It grabs the anything in the p, h1, h2, h3, h4, h5, h6, span and div tabs. (Tabs that generally include words)
         This function returns the list of words in that file.
         """
-        response = requests.get(url_content)
-        tree = html.fromstring(response.content)
-        text_elements = tree.xpath('//p | //h1 | //h2 | //h3 | //h4 | //h5 | //h6 | //span | //div')
-        words = []
-
-        for element in text_elements:
-            if element.text_content():
-                list_of_words = re.split(r'[\s\t\r\n]+', element.text_content())
-                for word in list_of_words:
-                    if ((not word.isspace()) and (len(word) >= 1)):
-                        words.append(word)
-        
-        return words
-            
-
+        return BeautifulSoup(url_data["content"]).get_text()
+    
+    
     def is_valid(self, url):
         """
         Function returns True or False based on whether the url has to be fetched or not. This is a great place to
@@ -177,8 +162,8 @@ class Crawler:
         # reducing links through parameters
         if len(query_params) > 5:
             return False
-        if not self.is_valid_path(parsed):
-            return False
+        # if not self.is_valid_path(parsed):
+        #     return False
         # reducing links based on url fragments
         if parsed.fragment in ["content-main"]:
             return False
@@ -186,7 +171,7 @@ class Crawler:
             return False
         try:
             if ( ".ics.uci.edu" in parsed.hostname \
-                   and not re.match(".*\.(css|js|bmp|gif|jpe?g|ico" + "|png|tiff?|mid|mp2|mp3|mp4" \
+                   and not re.match(r".*\.(css|js|bmp|gif|jpe?g|ico" + "|png|tiff?|mid|mp2|mp3|mp4" \
                                     + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf" \
                                     + "|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1" \
                                     + "|thmx|mso|arff|rtf|jar|csv" \
